f4869107514df5bb0da7be4e10e59ae5138ce409
Mag1cPanda <415391360@qq.com>
2016-03-28 16:15:24 +0800

    Update callx.js
    QQQQ

--- callx.js
+++ callx.js
@@ -4,7 +4,7 @@ defineClass('ViewController', {
     var view = self.ORIGgenView();
     view.setBackgroundColor(UIColor.greenColor())
     var label = UILabel.alloc().initWithFrame(view.bounds());
-    setTextAlignment(1);
+    label.setTextAlignment(1);
     label.setText("JSPatch");
     view.addSubview(label);
     return view;
