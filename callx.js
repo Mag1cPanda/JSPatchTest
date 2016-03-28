e72cf9961766b927aabe501be542aaa7cdd35813
Mag1cPanda <415391360@qq.com>
2016-03-28 15:59:34 +0800

    Update callx.js
    修改label的frame

--- callx.js
+++ callx.js
@@ -3,7 +3,7 @@ defineClass('ViewController', {
   genView: function() {
     var view = self.ORIGgenView();
     view.setBackgroundColor(UIColor.greenColor())
-    var label = UILabel.alloc().initWithFrame(view.frame());
+    var label = UILabel.alloc().initWithFrame(view.bounds());
     label.setText("JSPatch");
     view.addSubview(label);
     return view;
