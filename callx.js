ea65aa758d067fb1a33ef077827dfa4ff4cbba43
Mag1cPanda <415391360@qq.com>
2016-04-11 16:57:03 +0800

    Update callx.js

--- callx.js
+++ callx.js
@@ -1,1 +1,10 @@
-
+require('NSString,UIColor');
+defineClass('TestViewController', {
+    tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell");
+        cell.textLabel().setText(NSString.stringWithFormat("Cell%zi", indexPath.row()));
+        cell.contentView().setBackgroundColor(UIColor.blueColor());
+        cell.setSelectionStyle(UITableViewCellSelectionStyleNone);
+        return cell;
+    },
+});
