bed890c95605302ec91cd3952cb76a1fd6abecaf
Mag1cPanda <415391360@qq.com>
2016-04-11 16:53:36 +0800

    Update callx.js

--- callx.js
+++ callx.js
@@ -1,12 +1,1 @@
-require('UIView, UIColor, UILabel')
-defineClass('ViewController', {
-  genView: function() {
-    var view = self.ORIGgenView();
-    view.setBackgroundColor(UIColor.greenColor())
-    var label = UILabel.alloc().initWithFrame(view.bounds());
-    label.setTextAlignment(1);
-    label.setText("JSPatch");
-    view.addSubview(label);
-    return view;
-  }
-});
+
