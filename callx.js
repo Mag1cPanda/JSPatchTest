526307e0c6078153521fb7893887ae462a828168
Mag1cPanda <415391360@qq.com>
2016-04-11 16:58:07 +0800

    Update callx.js

--- callx.js
+++ callx.js
@@ -2,7 +2,7 @@ require('NSString,UIColor');
 defineClass('TestViewController', {
     tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
         var cell = tableView.dequeueReusableCellWithIdentifier("cell");
-        cell.textLabel().setText(NSString.stringWithFormat("Cell%zi", indexPath.row()));
+        cell.textLabel().setText(NSString.stringWithFormat("NewCell%zi", indexPath.row()));
         cell.contentView().setBackgroundColor(UIColor.blueColor());
         cell.setSelectionStyle(UITableViewCellSelectionStyleNone);
         return cell;
