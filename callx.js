a151b4b7b8e581ca1c2cf7605394b37a1d3c8412
Mag1cPanda <415391360@qq.com>
2016-04-11 16:50:52 +0800

    Update callx.js
    222

--- callx.js
+++ callx.js
@@ -10,13 +10,3 @@ defineClass('ViewController', {
     return view;
   }
 });
-
-defineClass('TestViewController', {
-    tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
-        var cell = tableView.dequeueReusableCellWithIdentifier("cell");
-        cell.textLabel().setText(NSString.stringWithFormat("Cell%zi", indexPath.row()));
-        cell.contentView().setBackgroundColor(UIColor.blueColor());
-        cell.setSelectionStyle(UITableViewCellSelectionStyleNone);
-        return cell;
-    },
-});
