e698a88ba5c81773ab90138a6300eed6fc44d767
Mag1cPanda <415391360@qq.com>
2016-04-12 09:08:42 +0800

    Update callx.js

--- callx.js
+++ callx.js
@@ -1,18 +1,43 @@
-require('UIView,UIColor,NSString');
-defineClass('ViewController', {
-    genView: function() {
-        var view = UIView.alloc().initWithFrame(CGRectMake(20, 100, 200, 200));
-        view.setBackgroundColor(UIColor.greenColor());
-        return view;
-    },
-});
+defineClass('JPViewController', {
+  handleBtn: function(sender) {
+    var tableViewCtrl = JPTableViewController.alloc().init()
+    self.navigationController().pushViewController_animated(tableViewCtrl, YES)
+  }
+})
 
-defineClass('TestViewController', {
-    tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
-        var cell = tableView.dequeueReusableCellWithIdentifier("cell");
-        cell.textLabel().setText(NSString.stringWithFormat("NewCell%zi", indexPath.row()));
-        cell.contentView().setBackgroundColor(UIColor.blueColor());
-        cell.setSelectionStyle(UITableViewCellSelectionStyleNone);
-        return cell;
-    },
-});
+defineClass('JPTableViewController : UITableViewController <UIAlertViewDelegate>', {
+  dataSource: function() {
+    var data = self.getProp('data')
+    if (data) return data;
+    var data = [];
+    for (var i = 0; i < 20; i ++) {
+      data.push("cell from js " + i);
+    }
+    self.setProp_forKey(data, 'data')
+    return data;
+  },
+  numberOfSectionsInTableView: function(tableView) {
+    return 1;
+  },
+  tableView_numberOfRowsInSection: function(tableView, section) {
+    return self.dataSource().count();
+  },
+  tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
+    var cell = tableView.dequeueReusableCellWithIdentifier("cell") 
+    if (!cell) {
+      cell = require('UITableViewCell').alloc().initWithStyle_reuseIdentifier(0, "cell")
+    }
+    cell.textLabel().setText(self.dataSource().objectAtIndex(indexPath.row()))
+    return cell
+  },
+  tableView_heightForRowAtIndexPath: function(tableView, indexPath) {
+    return 60
+  },
+  tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
+     var alertView = require('UIAlertView').alloc().initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("Alert",self.dataSource().objectAtIndex(indexPath.row()), self, "OK",  null);
+     alertView.show()
+  },
+  alertView_willDismissWithButtonIndex: function(alertView, idx) {
+    console.log('click btn ' + alertView.buttonTitleAtIndex(idx).toJS())
+  }
+})
